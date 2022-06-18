cfd6b64f40c07163364b2e7fcf88e57d30ec6a69
nir <nirrako@gmail.com>
2022-06-18 21:15:23 +0300

    Update bich2 to work with multi brackets inside a single segment

--- lib/bich2.rb
+++ lib/bich2.rb
@@ -4,9 +4,9 @@ class Bich2
   OLD_FOLDER    = 'old'
 
   BRACKETS_TYPES_H = {
-    round: {look: '()', regex: '\(.*\)'},
-    box:   {look: '[]', regex: '\[.*\]'},
-    curly: {look: '{}', regex: '\{.*\}'},
+    round: {look: '()', regex: '\(.*?\)'},
+    box:   {look: '[]', regex: '\[.*?\]'},
+    curly: {look: '{}', regex: '\{.*?\}'},
   }.with_indifferent_access
 
   Rule = Struct.new(:regex, :to, keyword_init: true)
@@ -68,71 +68,71 @@ class Bich2
     text.gsub(/<\w+(( \w+="[\w# ]+")+)?>/,'').gsub(/<\/\w+>/,'')
   end
 
-  private
+private
 
-    def create_rules(rules_h)
-      rules_h.each_with_object([]) do |(rule,args), rules|
-        rules.concat(self.send("get_#{rule}_rules", args))
-      end
+  def create_rules(rules_h)
+    rules_h.each_with_object([]) do |(rule,args), rules|
+      rules.concat(self.send("get_#{rule}_rules", args))
     end
+  end
 
-    def get_narrator_rules(narrator)
-      regex = case narrator
-      when :easy; /[A-Z -]+:( |\z|\r|\n)/
-      else /[\w -]+:( |\z|\r|\n)/
-      # else /[\w ]+: /
-      end
-      [Rule.new(regex: regex, to: '')]
+  def get_narrator_rules(narrator)
+    regex = case narrator
+    when :easy; /[A-Z -]+:( |\z|\r|\n)/
+    else /[\w -]+:( |\z|\r|\n)/
+    # else /[\w ]+: /
     end
+    [Rule.new(regex: regex, to: '')]
+  end
 
-    def get_brackets_types_rules(brackets_types)
-      regex = Regexp.new(brackets_types.map{|bracket_type| "(?:#{BRACKETS_TYPES_H[bracket_type][:regex]})"}.join('|'), Regexp::MULTILINE)
-      [Rule.new(regex: regex, to: '')]
+  def get_brackets_types_rules(brackets_types)
+    regex = Regexp.new(brackets_types.map{|bracket_type| "(?:#{BRACKETS_TYPES_H[bracket_type][:regex]})"}.join('|'), Regexp::MULTILINE)
+    [Rule.new(regex: regex, to: '')]
+  end
+
+  def convert_text(text)
+    text = Bich2.remove_css(text) if @remove_css
+    # (?:\d+\n\d\d:\d\d:\d\d,\d\d\d --> \d\d:\d\d:\d\d,\d\d\d\n)(.*?)(?:\n\n|\z)
+    # \d+\n\d\d:\d\d:\d\d,\d\d\d --> \d\d:\d\d:\d\d,\d\d\d\n(.*?)(?:\n\n|\z)
+    text.gsub(/(\d+(?:\r)?\n\d{2}:\d{2}:\d{2},\d{3} --> \d{2}:\d{2}:\d{2},\d{3}(?:\r)?\n)(.*?)(?=(?:(?:\r)?\n(?:\r)?\n|(?:\r)?\n\z))/m) do |m|
+      "#{$1}#{fix_content($2)}"
     end
+  end
 
-    def convert_text(text)
-      text = Bich2.remove_css(text) if @remove_css
-      # (?:\d+\n\d\d:\d\d:\d\d,\d\d\d --> \d\d:\d\d:\d\d,\d\d\d\n)(.*?)(?:\n\n|\z)
-      # \d+\n\d\d:\d\d:\d\d,\d\d\d --> \d\d:\d\d:\d\d,\d\d\d\n(.*?)(?:\n\n|\z)
-      text.gsub(/(\d+(?:\r)?\n\d{2}:\d{2}:\d{2},\d{3} --> \d{2}:\d{2}:\d{2},\d{3}(?:\r)?\n)(.*?)(?=(?:(?:\r)?\n(?:\r)?\n|(?:\r)?\n\z))/m) do |m|
-        "#{$1}#{fix_content($2)}"
-      end
-    end
+  # def fix_content(content)
+  #   rows_array = content.split("\n")
+  #   rows_array.map! do |line| # applying desired rules
+  #     # line = line.gsub(@brackets_regex,'') if @brackets_regex.present?
+  #     # line = line.gsub(/\[.*\]/,'')
+  #     # line = line.gsub(@narrator_regex,'')
+  #     @rules.each{|rule| line = line.gsub(rule.regex,rule.to)}
+  #     # @rules.each{|rule|
+  #     #   puts "line: #{line.inspect}"
+  #     #   line = line.gsub(rule.regex,rule.to)
+  #     # }
+  #
+  #     fix_spaces(line)
+  #   end.reject! do |line| # rejecting non words lines? (aka empty lines?)
+  #     # line !~ /[\wא-ת]/ # raised Encoding::CompatibilityError (UTF-8 regexp with ASCII-8BIT string)
+  #     line !~ /[[:alnum:]]/
+  #     # not(line =~ /[\w]/ || line =~ /[א-ת]/)
+  #   end
+  #   # puts "rows_array: #{rows_array.inspect}"
+  #   rows_array.compact.join("\n")
+  # end
 
-    # def fix_content(content)
-    #   rows_array = content.split("\n")
-    #   rows_array.map! do |line| # applying desired rules
-    #     # line = line.gsub(@brackets_regex,'') if @brackets_regex.present?
-    #     # line = line.gsub(/\[.*\]/,'')
-    #     # line = line.gsub(@narrator_regex,'')
-    #     @rules.each{|rule| line = line.gsub(rule.regex,rule.to)}
-    #     # @rules.each{|rule|
-    #     #   puts "line: #{line.inspect}"
-    #     #   line = line.gsub(rule.regex,rule.to)
-    #     # }
-    #
-    #     fix_spaces(line)
-    #   end.reject! do |line| # rejecting non words lines? (aka empty lines?)
-    #     # line !~ /[\wא-ת]/ # raised Encoding::CompatibilityError (UTF-8 regexp with ASCII-8BIT string)
-    #     line !~ /[[:alnum:]]/
-    #     # not(line =~ /[\w]/ || line =~ /[א-ת]/)
-    #   end
-    #   # puts "rows_array: #{rows_array.inspect}"
-    #   rows_array.compact.join("\n")
-    # end
+  def fix_content(content)
+    new_content = content
+    @rules.each{|rule| new_content = new_content.gsub(rule.regex,rule.to)}
+    # binding.pry
+    new_content.split("\n").reject do |line| # rejecting non words lines? (aka empty lines?)
+      line !~ /[[:alnum:]]/
+    end.compact.join("\n").squeeze(' ').strip
+  end
 
-    def fix_content(content)
-      new_content = content
-      @rules.each{|rule| new_content = new_content.gsub(rule.regex,rule.to)}
-      # binding.pry
-      new_content.split("\n").reject do |line| # rejecting non words lines? (aka empty lines?)
-        line !~ /[[:alnum:]]/
-      end.compact.join("\n").squeeze(' ').strip
-    end
-
-    def fix_spaces(line)
-      line.squeeze(' ').strip
-    end
+  def fix_spaces(line)
+    line.squeeze(' ').strip
+  end
 
 end
 
